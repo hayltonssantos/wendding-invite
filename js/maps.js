09b56e9a6134f2be359bfcb7ea780f70b7073009
Haylton Evangelista <59753574+hayltonssantos@users.noreply.github.com>
2023-01-18 17:19:34 -0300

    Add files via upload

---
+++ js/maps.js
@@ -0,0 +1,7 @@
+
+$(function(){
+	const maps = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49592.74010265681!2d-9.211295489652846!3d39.025662933075395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd18d459b286af23%3A0x617eb5366bd605aa!2sQuinta%20do%20Frade!5e0!3m2!1spt-BR!2spt!4v1673654254512!5m2!1spt-BR!2spt`
+
+	$('[mapsLocation]').attr(`src`,maps)
+
+})
---
+++ js/script.js
@@ -0,0 +1,122 @@
+
+
+//FUNCOES AQUI A ABAIXO--------------------
+function contarDias(dia){
+	let dataAtual = new Date()
+	let dataCasamento = new Date(dia)
+	
+	let timeDiff = dataCasamento.getTime() - dataAtual.getTime();
+	let contador = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+
+	let texto = ''
+
+	if(contador < 0 ){
+		texto = (`Casados a ${contador * -1} dias`)
+	}else if (contador == 0){
+		texto = (`É HOJE O GRANDE DIA`)
+	}else if (contador == 1){
+		texto = (`É AMANHÃ O GRANDE DIA`)
+	}else{
+		texto = (`Faltam ${contador} dias`)
+	}
+
+	//passar os dias para o HTML 
+
+	$('#dia').text(dataCasamento.getDate()+1)
+	$('#mes').text(dataCasamento.getMonth()+1)
+	$('#ano').text(((dataCasamento.getFullYear()).toString()).slice(2,4))
+	$('.contadorDias').text(texto)
+	$('#contadorDias2').text(texto)
+}
+
+function atribuiNome(nomeCasal,juntosDesde){
+	$('[nomeCasalJs]').text(nomeCasal)
+	$('[juntosDesde]').text(juntosDesde)
+}
+//FUNCOES AQUI A ABAIXO--------------------
+
+//Funcoes esconder descricao horario---------------
+function esconderDescricao(){
+	$('.descricaoHorario').hide()
+}
+
+function mostrarDescricao(elemento){
+	$(elemento).next().find('span').show()
+}
+//Funcoes esconder descricao horario-----------------
+
+
+//Funcoes add class foto-------------------------
+$('.photoHorario').mouseover(function(){
+	$(this).addClass("imgHover")
+	mostrarDescricao(this)
+})
+
+$('.photoHorario').mouseout(function(){
+	$(this).removeClass("imgHover")
+	esconderDescricao()
+})
+//Funcoes add class foto-------------------------
+
+$(function(){
+	$(`[verResultado]`).on('click',function(){
+		let q1 = $('[name=1q]:checked').val()
+		let q2 = $('[name=2q]:checked').val()
+		let q3 = $('[name=3q]:checked').val()
+		let q4 = $('[name=4q]:checked').val()
+
+		let res = 0
+
+		if (q1 == 'true'){
+			res = res + 1
+		}
+		if (q2 == 'true'){
+			res = res + 1
+		}
+		if (q3 == 'true'){
+			res = res + 1
+		}
+		if (q4 == 'true'){
+			res = res + 1
+		} 
+
+		$('[displayResult]').removeClass("d-none").addClass('d-flex')
+		$('[displayPerguntas]').addClass('d-none')
+		
+		$('[resultText]').text(`Você acertou ${res} perguntas!`)
+	})
+	$(`[voltar]`).on('click',function () {
+		$('[displayPerguntas]').removeClass("d-none")
+		$('[displayResult]').addClass('d-none').removeClass('d-flex')
+	})
+})
+
+
+//FUNCOES LOAD
+var diaCasamento = "2024-08-16"
+var nomeCasal = "Joana & Marco"
+var juntosDesde = "2022"
+var fotos = ['2','3','4']
+$(window).on('load',
+	contarDias(diaCasamento),
+	esconderDescricao(),
+	atribuiNome(nomeCasal,juntosDesde)
+	)
+
+$.each(fotos, function(index,value){
+		img = `img/img${value}.jpg`
+		$('[carouselImg]').append().after(`
+			<div class="carousel-item">
+                <img src="${img}" class="img-fluid imgsCarousel">
+            </div>
+
+		`)
+
+		$(`[carouselIndi]`).append().after(`
+			<li data-target="#carousel-indicadores" data-slide-to="${index+1}"></li>
+			`)
+	})
+//window.addEventListener('load',contarDias(diaCasamento))
+
+
+
